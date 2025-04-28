eba6589f6e4f43db11f15f839fbbcc45183a482f
andres8073562 <espineldavid56.@gmail.com>
2025-04-28 09:44:35 -0500

    feat : fire : My second commit

--- script/script.js
+++ script/script.js
@@ -1,4 +1,14 @@
-fetch(' http://api.weatherapi.com/v1')
-.then(response => response.json())
-.then(data => console.log(data));
+fetch(' http://api.weatherapi.com/v1/.json?key=[40f040b5a8f44270ad2120233252804[]&q=Floridablanca&lang=es')
+    .then(response => response.json())
+    .then(data => console.log(data));
 
+function Interface() {
+
+    let name = document.getElementById("name").value;
+    let  region = document.getElementById("region").value;
+    let country = document.getElementById("country").value;
+    let lat = document.getElementById("lat").value;
+    let lon = document.getElementById("lon").value;
+
+}
+Interface()
